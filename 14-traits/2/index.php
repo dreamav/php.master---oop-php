e4c595872677c10a07285c8b7128948e2e424efc
Mevericks <reloved@gmail.com>
2018-09-05 10:22:35 +0300

    трейты

---
+++ 14-traits/2/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use app\BookProduct;
+use app\NotebookProduct;
+use PHPMailer\PHPMailer\PHPMailer;
+use wfm\interfaces\iGadget;
+
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+require __DIR__ . "/vendor/autoload.php";
+
+function debug($data){
+    echo '<pre>' . print_r($data,1) . '</pre>';
+}
+
+function offerCase(iGadget $product){
+	echo "<p>Предлагаем чехлы для гаджета {$product->getName()}</p>";
+}
+
+$book = new BookProduct('Три мушкетера', 20, 1000);
+$notebook = new NotebookProduct('Vaio',400, 'intel');
+
+debug($book);
+echo "<br>";
+debug($notebook);
