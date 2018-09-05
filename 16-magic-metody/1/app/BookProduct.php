f1bfa1aa2ca90fc1789ba3d0d7a8ab834c71582c
Mevericks <reloved@gmail.com>
2018-09-05 11:48:57 +0300

    магические методы init

---
+++ 16-magic-metody/1/app/BookProduct.php
@@ -0,0 +1,65 @@
+<?php
+namespace app;
+
+use wfm\interfaces\i3DBook;
+use wfm\Product;
+
+class BookProduct extends Product implements i3DBook
+{
+
+    public $numPages;
+
+    public $action1;
+    public $action2;
+
+    public function __construct($name, $price, $numPages)
+    {
+        parent::__construct($name, $price);
+        $this->numPages = $numPages;
+        $this->setDiscount(5);
+//        var_dump(self::class);
+    }
+
+    public function __toString()
+    {
+//        return "Hello Book";
+        return $this->getProduct();
+    }
+
+    public function getProduct()
+    {
+        $out = parent::getProduct();
+        $out .= "Цена без скидки: {$this->price}<br>";
+        $out .= "Кол-во страниц: {$this->numPages}<br>";
+        return $out;
+    }
+
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+
+    public function test()
+    {
+        // TODO: Implement test() method.
+        var_dump(self::TEST);
+    }
+
+    public function addProduct($name, $price, $numPages = 0)
+    {
+        // TODO: Implement addProduct() method.
+    }
+
+    public function doAction1()
+    {
+        echo $this->action1 = "<p>выполнили действие 1</p>";
+        return $this;
+    }
+
+    public function doAction2()
+    {
+        echo $this->action1 = "<p>выполнили действие 2</p>";
+        return $this;
+    }
+
+}
---
+++ 16-magic-metody/1/vendor/wfm/traits/tColor.php
@@ -0,0 +1,25 @@
+<?php
+namespace wfm\traits;
+
+trait tColor
+{
+
+	private $color;
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+}
